65e328ac264de42f5009c50e18ca2a57fb1a67ab
CRISTIAN JOSUE BARRIENTOS MIJANGOS <cjbarrientosmi@correo.url.edu.gt>
2020-02-06 11:38:43 -0600

    Proyecto Base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Est_Datos_LAB_00.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
